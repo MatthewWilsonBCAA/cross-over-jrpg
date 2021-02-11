60fe19efbfb408351935e0f02402d2cc2470c410
MatthewWilsonBCAA <mwilson21@basecampcodingacademy.org>
2021-02-11 15:36:12 -0600

    made camera movement speed a constant, changed world around a bit more. also added a window caption

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import copy
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+pygame.display.set_caption("Cross-over JRPG")
+#icon = pygame.image.load()
+#pygame.display.set_icon(icon)
+
 running = True
 
 player = Player((SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
@@ -21,49 +25,49 @@ while running:
     screen.fill((0, 0, 0))
     pressed_keys = pygame.key.get_pressed()
     if pressed_keys[K_w]:
-        screen_fustrum[0][1] += 2
-        screen_fustrum[1][1] += 2
+        screen_fustrum[0][1] += MOVEMENT_SPEED
+        screen_fustrum[1][1] += MOVEMENT_SPEED
         for wall in walls:
             temp = copy.copy(wall)
             temp.rect = wall.rect.copy()
             temp.rect = temp.surf.get_rect(center=(temp.rect.center[0] + screen_fustrum[0][0], temp.rect.center[1] + screen_fustrum[0][1]))
             HITS = pygame.sprite.collide_rect(player, temp)
             if HITS:
-                screen_fustrum[0][1] -= 2
-                screen_fustrum[1][1] -= 2
+                screen_fustrum[0][1] -= MOVEMENT_SPEED
+                screen_fustrum[1][1] -= MOVEMENT_SPEED
     if pressed_keys[K_s]:
-        screen_fustrum[0][1] -= 2
-        screen_fustrum[1][1] -= 2
+        screen_fustrum[0][1] -= MOVEMENT_SPEED
+        screen_fustrum[1][1] -= MOVEMENT_SPEED
         for wall in walls:
             temp = copy.copy(wall)
             temp.rect = wall.rect.copy()
             temp.rect = temp.surf.get_rect(center=(temp.rect.center[0] + screen_fustrum[0][0], temp.rect.center[1] + screen_fustrum[0][1]))
             HITS = pygame.sprite.collide_rect(player, temp)
             if HITS:
-                screen_fustrum[0][1] += 2
-                screen_fustrum[1][1] += 2
+                screen_fustrum[0][1] += MOVEMENT_SPEED
+                screen_fustrum[1][1] += MOVEMENT_SPEED
     if pressed_keys[K_a]:
-        screen_fustrum[0][0] += 2
-        screen_fustrum[1][0] += 2
+        screen_fustrum[0][0] += MOVEMENT_SPEED
+        screen_fustrum[1][0] += MOVEMENT_SPEED
         for wall in walls:
             temp = copy.copy(wall)
             temp.rect = wall.rect.copy()
             temp.rect = temp.surf.get_rect(center=(temp.rect.center[0] + screen_fustrum[0][0], temp.rect.center[1] + screen_fustrum[0][1]))
             HITS = pygame.sprite.collide_rect(player, temp)
             if HITS:
-                screen_fustrum[0][0] -= 2
-                screen_fustrum[1][0] -= 2
+                screen_fustrum[0][0] -= MOVEMENT_SPEED
+                screen_fustrum[1][0] -= MOVEMENT_SPEED
     if pressed_keys[K_d]:
-        screen_fustrum[0][0] -= 2
-        screen_fustrum[1][0] -= 2
+        screen_fustrum[0][0] -= MOVEMENT_SPEED
+        screen_fustrum[1][0] -= MOVEMENT_SPEED
         for wall in walls:
             temp = copy.copy(wall)
             temp.rect = wall.rect.copy()
             temp.rect = temp.surf.get_rect(center=(temp.rect.center[0] + screen_fustrum[0][0], temp.rect.center[1] + screen_fustrum[0][1]))
             HITS = pygame.sprite.collide_rect(player, temp)
             if HITS:
-                screen_fustrum[0][0] += 2
-                screen_fustrum[1][0] += 2
+                screen_fustrum[0][0] += MOVEMENT_SPEED
+                screen_fustrum[1][0] += MOVEMENT_SPEED
     #print(screen_fustrum)
     for event in pygame.event.get():
         if event.type == QUIT:
